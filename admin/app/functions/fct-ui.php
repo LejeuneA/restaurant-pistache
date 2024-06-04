a18e026a6442db294a52ef19b5ece4a772a403c7
A.Lejeune <acelyalejeune@gmail.com>
2024-06-04 13:46:55 +0200

    Update fct-ui.php

--- admin/app/functions/fct-ui.php
+++ admin/app/functions/fct-ui.php
@@ -90,21 +90,24 @@ function displayHeadSection($title = APP_NAME)
     $head = '
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Découvrez Librairie Lejeune pour des livres, fournitures de papeterie et cadeaux uniques. Parcourez notre sélection dès aujourd\'hui!">
+    <meta name="description" content="Restaurant Pistache - Liége">
 
-    <!-- Custom Sass file -->
-    <link rel="stylesheet" href="'.DOMAIN.'/css/styles.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="'.DOMAIN.'/css/styles.css">
+
+    <!-- Swiper CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+    <!-- AOS Library CSS -->
+    <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
 
     <!-- Favicon -->
-	<link rel="icon" type="image/png" href="../assets/icons/favicon.png">
+    <link rel="icon" type="image/png" href="../assets/icons/favicon.png">
 
-    <!-- Favicon -->
-	<link rel="icon" type="image/png" href="assets/icons/favicon.png">
-
-    <!-- Google Fonts Preconnect and Link -->
+    <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Chelsea+Market&family=Great+Vibes&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
         
     <title>' . $title . '</title>
     ';
@@ -319,203 +322,6 @@ function displayNavigation()
 
 
 /**-----------------------------------------------------------------
-                    Navigation customer
-*------------------------------------------------------------------**/
-
-/**
- * Affichage de la navigation
- * 
- * @return void 
- */
-function displayNavigationCustomer()
-{
-
-    $navigation = '';
-
-    if ($_SESSION['IDENTIFY']) {
-        $navigation .= '
-        <nav class="navbar">
-        <div class="navbar-container container">
-            <!-- Logo -->
-            <a href="../index.php">
-                <img src="../assets/logo/librairie-lejeune.png" class="navbar-brand-img" alt="Librairie Lejeune Logo">
-            </a>
-            <!-- Logo end -->
-
-            <!-- Right-side content -->
-            <div class="d-flex">
-                <!-- Social icons -->
-                <ul class="social-nav">
-                    <!-- Icons -->
-                    <li class="social-item">
-                        <a class="social-link" href="https://www.facebook.com/">
-                            <i class="fa-brands fa-square-facebook fa-lg"></i>
-                        </a>
-                    </li>
-                    <li class="social-item">
-                        <a class="social-link" href="https://twitter.com/">
-                            <i class="fa-brands fa-x-twitter fa-lg"></i>
-                        </a>
-                    </li>
-                    <li class="social-item">
-                        <a class="social-link" href="https://www.instagram.com">
-                            <i class="fa-brands fa-instagram fa-lg"></i>
-                        </a>
-                    </li>
-                </ul>
-                <!-- Social icons end -->
-
-                <!-- Search -->
-					<form class="search" role="search" action="search.php" method="GET">
-						<div class="search-group">
-							<input class="form-control" type="search" name="query" placeholder="Que cherchez-vous?" aria-label="Search">
-							<button class="btn-search" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-						</div>
-					</form>
-				<!-- Search end -->
-
-                <!-- Login button -->
-                <a href="logoff.php" class="btn-primary">Déconnexion</a>
-                <!-- Login button end -->
-            </div>
-            <!-- Right-side content end -->
-        </div>
-    </nav>
-    <!---------------------------------------------------------------
-                                     Menu
-    ----------------------------------------------------------------->
-    <div class="navbar-menu">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="../index.php">Accueil</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="../public/livres.php">Livres</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="../public/papeteries.php">Papeterie</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="../public/cadeaux.php">Cadeaux</a>
-            </li>
-        </ul>
-    </div>
-    <!---------------------------------------------------------------
-                                 Menu end
-    ---------------------------------------------------------------->
-
-    <!---------------------------------------------------------------
-                             Offcanvas menu
-    ----------------------------------------------------------------->
-    <div id="mySidenav" class="sidenav">
-
-        <!-- Search -->
-            <form class="search" role="search" action="search.php" method="GET">
-                <div class="search-group">
-                 <input class="form-control" type="search" name="query" placeholder="Que cherchez-vous?" aria-label="Search">
-                 <button class="btn-search" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-                </div>
-            </form>
-        <!-- Search end -->
-
-        <!-- Menu -->
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        <a class="nav-link" href="../index.php">Accueil</a>
-        <a class="nav-link" href="../public/livres.php">Livres</a>
-        <a class="nav-link" href="../public/papeteries.php">Papeterie</a>
-        <a class="nav-link" href="../public/cadeaux.php">Cadeaux</a>
-        <!-- Menu end -->
-
-        <!-- Login button -->
-        <a href="logoff.php" class="btn-login">Déconnexion</a>
-        <!-- Login button end -->
-
-        <!-- Social icons -->
-        <ul class="social-nav">
-            <!-- Icons -->
-            <li class="social-item">
-                <a class="social-link" href="https://www.facebook.com/">
-                    <i class="fa-brands fa-square-facebook fa-lg"></i>
-                </a>
-            </li>
-            <li class="social-item">
-                <a class="social-link" href="https://twitter.com/">
-                    <i class="fa-brands fa-x-twitter fa-lg"></i>
-                </a>
-            </li>
-            <li class="social-item">
-                <a class="social-link" href="https://www.instagram.com">
-                    <i class="fa-brands fa-instagram fa-lg"></i>
-                </a>
-            </li>
-        </ul>
-        <!-- Social icons end -->
-    </div>
-
-    <!-- Hamburger icon for smaller screens -->
-    <div class="navbar-hamburger">
-        <div id="hamburger" onclick="openNav()"><i class="fa-solid fa-bars"></i></div>
-    </div>
-    <!------------------------------------------------------------- 
-                          Offcanvas menu end
-    --------------------------------------------------------------->
-        ';
-    } else {
-        $navigation .= '
-        <nav class="navbar">
-        <div class="navbar-container container">
-            <!-- Logo -->
-            <a href="../index.php">
-                <img src="../assets/logo/librairie-lejeune.png" class="navbar-brand-img" alt="Librairie Lejeune Logo">
-            </a>
-            <!-- Logo end -->
-
-            <!-- Right-side content -->
-            <div class="d-flex">
-                <!-- Social icons -->
-                <ul class="social-nav">
-                    <!-- Icons -->
-                    <li class="social-item">
-                        <a class="social-link" href="https://www.facebook.com/">
-                            <i class="fa-brands fa-square-facebook fa-lg"></i>
-                        </a>
-                    </li>
-                    <li class="social-item">
-                        <a class="social-link" href="https://twitter.com/">
-                            <i class="fa-brands fa-x-twitter fa-lg"></i>
-                        </a>
-                    </li>
-                    <li class="social-item">
-                        <a class="social-link" href="https://www.instagram.com">
-                            <i class="fa-brands fa-instagram fa-lg"></i>
-                        </a>
-                    </li>
-                </ul>
-                <!-- Social icons end -->
-
-                <!-- Search -->
-                <form class="search" role="search">
-                    <div class="search-group">
-                        <input class="form-control" type="search" placeholder="Que cherhez-vous?" aria-label="Search">
-                        <button class="btn-search" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-                    </div>
-                </form>
-                <!-- Search end -->
-
-                <!-- Login button -->
-                <a href="login.php" class="btn-primary">Se connecter</a>
-                <!-- Login button end -->
-            </div>
-            <!-- Right-side content end -->
-        </div>
-    </nav>';
-    }
-
-    echo $navigation;
-}
-
-
-/**-----------------------------------------------------------------
                   Retour d'un message au format HTML
  *------------------------------------------------------------------**/
 
@@ -535,56 +341,34 @@ function getMessage($message, $type = 'success')
         Generate HTML markup for displaying articles information
  *------------------------------------------------------------------**/
 /**
- * Generate HTML markup for displaying livre information
+ * Generate HTML markup for displaying starters information
  * 
- * @param array $livre 
+ * @param array $starter 
  * @return string 
  */
-function generateLivreHTML($livre)
+function generateStarterHTML($starter)
 {
-    // Start building the HTML markup
-    $html = '<article class="article-container">';
-    $html .= '<div class="product-img">';
-    $html .= '<a href="'.DOMAIN.'/public/product-livre.php?idLivre=' . $livre['idLivre'] . '">';
-    $html .= '<img src="'.DOMAIN.'/'.$livre['image_url'] . '" alt="' . $livre['title'] . '">';
-    $html .= '</a>';
-    $html .= '</div>';
+    $html = ""; // Initialize empty HTML string
 
-    $html .= '<div class="product-info">';
-    // Check if 'title', 'writer', and 'feature' keys are set before accessing them
-    $title = isset($livre['title']) ? $livre['title'] : 'Titre non disponible';
-    $writer = isset($livre['writer']) ? $livre['writer'] : 'Auteur non disponible';
-    $feature = isset($livre['feature']) ? $livre['feature'] : 'Feature non disponible';
+    // Sanitize data to prevent XSS vulnerabilities (optional but recommended)
+    $imageUrl = htmlspecialchars($starter['imageUrl'], ENT_QUOTES);
+    $title = htmlspecialchars($starter['title'], ENT_QUOTES);
+    $price = htmlspecialchars($starter['price'], ENT_QUOTES);
+    $description = htmlspecialchars($starter['description'], ENT_QUOTES);
 
-    $html .= '<a href="'.DOMAIN.'/public/product-livre.php?idLivre=' . $livre['idLivre'] . '">';
-    $html .= '<h2>' . $title . '</h2>';
-    $html .= '</a>';
-    $html .= '<p>' . $writer . ' <span>' . $feature . '</span></p>';
-
-    // Check if 'content' key is set before accessing it
-    if (isset($livre['content'])) {
-
-        $truncatedContent = strlen($livre['content']) > 350 ? substr($livre['content'], 0, 350) . '...' : $livre['content'];
-        $html .= '<div class="product-description">';
-        $html .= '<p>' . htmlspecialchars_decode($truncatedContent) . '</p>';
-        $html .= '</div>';
-    }
-
-    $html .= '<div class="more-info">';
-    // Check if 'idLivre' key is set before creating the link
-    $link = isset($livre['idLivre']) ? ''.DOMAIN. '/public/product-livre.php?idLivre=' . $livre['idLivre'] : '#';
-    $html .= '<a href="' . $link . '">Savoir plus</a>';
-    $html .= '</div>';
-    $html .= '</div>';
-
-    $html .= '<div class="product-price">';
-    // Check if 'price' key is set before accessing it
-    $price = isset($livre['price']) ? $livre['price'] . ' €' : 'Prix non disponible';
-    $html .= '<p>' . $price . ' <span><i class="fas fa-truck"></i> Livraison 1 à 2 semaines</span><span><i class="fas fa-receipt"></i> Retrait en magasin dans 2 h.</span></p>';
-    $html .= '<a href="#" class="btn-primary"><i class="fas fa-shopping-cart"></i> Ajouter au panier</a>';
-    $html .= '</div>';
-
-    $html .= '</article>';
+    // Construct the HTML markup
+    $html .= "
+        <div class=\"menu-items\" data-aos=\"fade-up\" data-aos-delay=\"150\">
+            <div class=\"menu-item\">
+                <img class=\"menu-item-image\" src=\"$imageUrl\" alt=\"$title\">
+                <div class=\"menu-item-info\">
+                    <h3 class=\"menu-item-title\">$title</h3>
+                    <span class=\"menu-item-price\">€$price</span>
+                    <p>$description</p>
+                </div>
+            </div>
+        </div>
+    ";
 
     return $html;
 }
