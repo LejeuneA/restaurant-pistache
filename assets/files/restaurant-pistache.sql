f991a0d6d962a0a1aa37b83b0aa379177b99387f
A.Lejeune <acelyalejeune@gmail.com>
2024-06-07 11:29:59 +0200

    Update restaurant-pistache.sql

--- assets/files/restaurant-pistache.sql
+++ assets/files/restaurant-pistache.sql
@@ -139,7 +139,7 @@ CREATE TABLE `users` (
   `passwd` varchar(250) NOT NULL,
   `permission` int DEFAULT NULL,
   PRIMARY KEY (`idUser`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -148,7 +148,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'lejeune@mail.com','130580',1);
+INSERT INTO `users` VALUES (1,'lejeune@mail.com','130580',1),(2,'guest@mail.com','123456',2);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -161,4 +161,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-07 11:26:53
+-- Dump completed on 2024-06-07 11:29:51
