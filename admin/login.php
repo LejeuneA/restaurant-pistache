d32b10ab2dedef261515a2f1ee85d4bc4f225c2b
A.Lejeune <acelyalejeune@gmail.com>
2024-06-08 18:11:44 +0200

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -119,8 +119,9 @@ if (!is_object($conn)) {
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js" integrity="sha512-u3fPA7V8qQmhBPNT5quvaXVa1mnnLSXUep5PS1qo5NRzHwG19aHmNJnj1Q8hpA/nBWZtZD4r4AX6YOt5ynLN2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-    <!-- Include functions.js -->
-    <script src="../js/functions.js"></script>
+   <!-- Main Js -->
+   <script src="../js/main.js"></script>
+   
 </body>
 
 </html>
