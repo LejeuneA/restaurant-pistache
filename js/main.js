1cbad886927fb450d1d471742816bb8b0b2ef2af
A.Lejeune <acelyalejeune@gmail.com>
2024-06-08 20:00:35 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -147,12 +147,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
 /* -------------------------------------------------------
 					Image preview
-		---------------------------------------------------------*/
+---------------------------------------------------------*/
 function previewImage(input) {
 	var preview = document.getElementById('image_preview');
 	if (input.files && input.files[0]) {
 		var reader = new FileReader();
-		reader.onload = function (e) {
+		reader.onload = function(e) {
 			preview.src = e.target.result;
 		}
 		reader.readAsDataURL(input.files[0]);
