093255e205b1f7bab6d9a0624b354c52788cdbd7
A.Lejeune <acelyalejeune@gmail.com>
2024-06-08 18:11:26 +0200

    Update article-maincourse.php

--- admin/article-maincourse.php
+++ admin/article-maincourse.php
@@ -49,14 +49,18 @@ if (isset($_GET['idPapeterie']) && !empty($_GET['idPapeterie'])) {
             ?>
         </div>
     </div>
+    
     <!-- Footer -->
     <footer>
         <div data-include="footer"></div>
     </footer>
+
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js" integrity="sha512-u3fPA7V8qQmhBPNT5quvaXVa1mnnLSXUep5PS1qo5NRzHwG19aHmNJnj1Q8hpA/nBWZtZD4r4AX6YOt5ynLN2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <!-- Include functions.js -->
-    <script src="../js/functions.js"></script>
+
+    <!-- Main Js -->
+    <script src="../js/main.js"></script>
+
 </body>
 
 </html>
