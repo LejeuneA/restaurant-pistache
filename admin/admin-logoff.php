6bce0a04ce254ce6fe415a85ffe9e36ab7d2263e
A.Lejeune <acelyalejeune@gmail.com>
2024-06-08 18:11:57 +0200

    Update admin-logoff.php

--- admin/admin-logoff.php
+++ admin/admin-logoff.php
@@ -29,7 +29,7 @@ if (!is_object($conn)) {
 
             // Appel de la fonction d'identification
             // Utiliser cette fonction si les mots de passe sont en clair dans la DB
-            $user = userIdentificationDB($conn, $datas);          
+            $user = userIdentificationDB($conn, $datas);
 
             // On vérifie si on a une adresse email dans le tableau $user, si c'est le cas on est connecté
             $connexionSuccessfull = !empty($user['email']);
@@ -119,9 +119,10 @@ if (!is_object($conn)) {
 
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js" integrity="sha512-u3fPA7V8qQmhBPNT5quvaXVa1mnnLSXUep5PS1qo5NRzHwG19aHmNJnj1Q8hpA/nBWZtZD4r4AX6YOt5ynLN2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    
+    <!-- Main Js -->
+    <script src="../js/main.js"></script>
 
-    <!-- Include functions.js -->
-    <script src="../js/functions.js"></script>
 </body>
 
 </html>
