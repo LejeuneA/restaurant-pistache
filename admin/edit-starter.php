07e6546cc86caacac68ca4771076cf78c06670f8
A.Lejeune <acelyalejeune@gmail.com>
2024-06-08 19:04:25 +0200

    Update edit-starter.php

--- admin/edit-starter.php
+++ admin/edit-starter.php
@@ -177,7 +177,7 @@ if (isset($_SESSION['message'])) {
                         <div class="form-ctrl">
                             <label for="price" class="form-ctrl">Price</label>
                             <input type="text" class="form-ctrl" id="price" name="price" value="<?php echo isset($starter['price']) ? $starter['price'] : ''; ?>">
-                        </div> 
+                        </div>
 
                     </div>
 
@@ -219,10 +219,15 @@ if (isset($_SESSION['message'])) {
     displayJSSection($tinyMCE);
     ?>
 
-    <!-- Footer -->
+    <!-----------------------------------------------------------------
+                               Footer
+    ------------------------------------------------------------------>
     <footer>
-        <div data-include="footer"></div>
+        <?php displayFooter(); ?>
     </footer>
+    <!-----------------------------------------------------------------
+                                   Footer end
+    ------------------------------------------------------------------>
 
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js" integrity="sha512-u3fPA7V8qQmhBPNT5quvaXVa1mnnLSXUep5PS1qo5NRzHwG19aHmNJnj1Q8hpA/nBWZtZD4r4AX6YOt5ynLN2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
