647654d753a216849c2605bcae15864100424fe0
A.Lejeune <acelyalejeune@gmail.com>
2024-06-10 10:37:01 +0200

    Update article-maincourse.php

--- admin/article-maincourse.php
+++ admin/article-maincourse.php
@@ -39,72 +39,7 @@ if (isset($_GET['idMainCourse']) && !empty($_GET['idMainCourse'])) {
                                 Header
     ------------------------------------------------------------------>
     <header class="header-article">
-        
-        <!---------------------------------------------------------------
-                                Navigation
-        ---------------------------------------------------------------->
-        <nav class="navbar">
-            <div class="navbar-container container">
-                <!-- Logo -->
-                <a class="navbar-brand" href="../index.php">
-                    Pistache
-                </a>
-                <!-- Logo end -->
-
-                <!-- Right-side content -->
-                <div class="navbar-right">
-                    <!-- Navbar menu -->
-                    <div class="navbar-menu">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link" href="../index.php">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="../index.php#about">About</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="../public/menu.php">Menu</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="../public/contact.php">Contact</a>
-                            </li>
-                        </ul>
-
-                        <!-- Login button -->
-                        <a href="../public/reservation.php" class="btn-primary">Book a table</a>
-                        <!-- Login button end -->
-                    </div>
-                    <!-- Navbar menu end -->
-                </div>
-                <!-- Right-side content end -->
-            </div>
-        </nav>
-        <!-- End Nav Menu -->
-
-        <!---------------------------------------------------------------
-                             Offcanvas menu
-        ----------------------------------------------------------------->
-        <div id="mySidenav" class="sidenav">
-
-            <!-- Menu -->
-            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-            <a class="nav-link" href="./index.php">Home</a>
-            <a class="nav-link" href="./index.php#about">About</a>
-            <a class="nav-link" href="../public/menu.php">Menu</a>
-            <a class="nav-link" href="../public/contact.php">Contact</a>
-            <!-- Menu end -->
-
-            <!-- Login button -->
-            <a href="../public/reservation.php" class="btn-primary">Book a table</a>
-            <!-- Login button end -->
-        </div>
-
-        <!-- Hamburger icon for smaller screens -->
-        <div class="navbar-hamburger">
-            <div id="hamburger" onclick="openNav()"><i class="fas fa-bars"></i></div>
-        </div>
-        <!-- Offcanvas menu end -->
-        <!-- Navigation end-->
+        <?php displayNavigationArticle(); ?>
     </header>
     <!-----------------------------------------------------------------
                             Header end
@@ -139,10 +74,10 @@ if (isset($_GET['idMainCourse']) && !empty($_GET['idMainCourse'])) {
 
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js" integrity="sha512-u3fPA7V8qQmhBPNT5quvaXVa1mnnLSXUep5PS1qo5NRzHwG19aHmNJnj1Q8hpA/nBWZtZD4r4AX6YOt5ynLN2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    
+
     <!-- Main JS -->
     <script src="../js/main.js"></script>
-    
+
 </body>
 
 </html>
