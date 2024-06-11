1f26de933460677d5e8cf2d1bd10671c4b33860d
A.Lejeune <acelyalejeune@gmail.com>
2024-06-11 09:44:35 +0200

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,38 +1,45 @@
 <?php
-$firstName = $_POST['firstName'];
-$lastName = $_POST['lastName'];
-$email = $_POST['email']; // Assuming 'email' corresponds to 'email' in your HTML form
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+
+const DOMAIN = 'http://localhost/restaurant-pistache';
+
+$firstName = isset($_POST['firstName']) ? $_POST['firstName'] : null;
+$lastName = isset($_POST['lastName']) ? $_POST['lastName'] : null;
+$email = isset($_POST['email']) ? $_POST['email'] : null;
+$phone = isset($_POST['phone']) ? $_POST['phone'] : null;
+$message = isset($_POST['message']) ? $_POST['message'] : null;
+
+// Check for missing required fields
+if (!$firstName || !$lastName || !$email || !$phone || !$message) {
+    die('Please fill in all required fields.');
+}
 
 // Database connection
-$conn = new mysqli('mysql.acelyalejeune.be', 'acelyalejeune', '@NtLYa130580', 'portfolio_contact_form');
+$conn = new mysqli('localhost', 'root', '@NtLYa130580', 'restaurant-pistache');
 if ($conn->connect_error) {
     die('Connection Failed: ' . $conn->connect_error);
 } else {
     // Insert form data into the database
-    $stmt = $conn->prepare("INSERT INTO contact_info (firstName, lastName, email, subject, message) VALUES (?, ?, ?, ?, ?)");
-    $stmt->bind_param("sssss", $firstName, $lastName, $email, $subject, $message);
+    $stmt = $conn->prepare("INSERT INTO contact (firstName, lastName, email, phone, message) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssss", $firstName, $lastName, $email, $phone, $message);
     $stmt->execute();
 
     // Send email notification
-    $to = "acelyalejeune@gmail.com"; 
-    $subject = "New Form Submission";
-    $body = "You have received a new form submission:\n\n";
-    $body .= "Name: $firstName $lastName\n";
-    $body .= "Email: $email\n";
-    $body .= "Subject: $subject\n";
-    $body .= "Message: $message\n";
+    // $to = "acelyalejeune@gmail.com"; 
+    // $subject = "New Form Submission";
+    // $body = "You have received a new form submission:\n\n";
+    // $body .= "Name: $firstName $lastName\n";
+    // $body .= "Email: $email\n";
+    // $body .= "Phone: $phone\n";
+    // $body .= "Message: $message\n";
 
-    // Send email using mail() function
-    mail($to, $subject, $body);
+    // // Send email using mail() function
+    // mail($to, $subject, $body);
 
     echo "Message sent...";
 
-    // Redirect back to index.html after 3 seconds
-    header("refresh:1;url=https://acelyalejeune.be/");
+    // Redirect back to index.html after 1 second
+    header("refresh:2;url=" . DOMAIN);
 
     $stmt->close();
     $conn->close();
 }
-
