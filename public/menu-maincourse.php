3d5fb86cf4365e928917730a44a2fc732273e69d
A.Lejeune <acelyalejeune@gmail.com>
2024-06-11 13:23:59 +0200

    Update menu-maincourse.php

--- public/menu-maincourse.php
+++ public/menu-maincourse.php
@@ -49,7 +49,7 @@ if (!is_object($conn)) {
     <main>
         <!-- Hero section -->
         <section class="hero-section hero-maincourse">
-            <h2>Explore Our Culinary Delights</h2>
+            <h2>Savor Our Signature Dishes</h2>
             <div class="hero-container container">
             </div>
         </section>
