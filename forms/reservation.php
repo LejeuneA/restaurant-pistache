62a6417eabaf391d4048fdff1cfbb0060b5b86ac
A.Lejeune <acelyalejeune@gmail.com>
2024-06-11 09:44:38 +0200

    Update reservation.php

--- forms/reservation.php
+++ forms/reservation.php
@@ -1,36 +1,47 @@
 <?php
-$firstName = $_POST['firstName'];
-$lastName = $_POST['lastName'];
-$email = $_POST['email']; // Assuming 'email' corresponds to 'email' in your HTML form
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+
+const DOMAIN = 'http://localhost/restaurant-pistache';
+
+$name = isset($_POST['name']) ? $_POST['name'] : null;
+$email = isset($_POST['email']) ? $_POST['email'] : null;
+$phone = isset($_POST['phone']) ? $_POST['phone'] : null;
+$book_date = isset($_POST['book_date']) ? $_POST['book_date'] : null;
+$book_time = isset($_POST['book_time']) ? $_POST['book_time'] : null;
+$person = isset($_POST['person']) ? $_POST['person'] : null;
+
+// Check for missing required fields
+if (!$name || !$email || !$book_date || !$book_time || !$person) {
+    die('Please fill in all required fields.');
+}
 
 // Database connection
-$conn = new mysqli('mysql.acelyalejeune.be', 'acelyalejeune', '@NtLYa130580', 'portfolio_contact_form');
+$conn = new mysqli('localhost', 'root', '@NtLYa130580', 'restaurant-pistache');
 if ($conn->connect_error) {
     die('Connection Failed: ' . $conn->connect_error);
 } else {
     // Insert form data into the database
-    $stmt = $conn->prepare("INSERT INTO contact_info (firstName, lastName, email, subject, message) VALUES (?, ?, ?, ?, ?)");
-    $stmt->bind_param("sssss", $firstName, $lastName, $email, $subject, $message);
+    $stmt = $conn->prepare("INSERT INTO reservation (name, email, phone, book_date, book_time, person) VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssssi", $name, $email, $phone, $book_date, $book_time, $person);
     $stmt->execute();
 
     // Send email notification
-    $to = "acelyalejeune@gmail.com"; 
-    $subject = "New Form Submission";
-    $body = "You have received a new form submission:\n\n";
-    $body .= "Name: $firstName $lastName\n";
-    $body .= "Email: $email\n";
-    $body .= "Subject: $subject\n";
-    $body .= "Message: $message\n";
+    // $to = "acelyalejeune@gmail.com"; 
+    // $subject = "New Reservation";
+    // $body = "You have received a new reservation:\n\n";
+    // $body .= "Name: $name\n";
+    // $body .= "Email: $email\n";
+    // $body .= "Phone: $phone\n";
+    // $body .= "Date: $book_date\n";
+    // $body .= "Time: $book_time\n";
+    // $body .= "Number of People: $person\n";
 
-    // Send email using mail() function
-    mail($to, $subject, $body);
+    // // Send email using mail() function
+    // mail($to, $subject, $body);
 
-    echo "Message sent...";
+    echo "Reservation made successfully...";
 
-    // Redirect back to index.html after 3 seconds
-    header("refresh:1;url=https://acelyalejeune.be/");
+    // Redirect back to index.html after 1 second
+    header("refresh:2;url=" . DOMAIN);
 
     $stmt->close();
     $conn->close();
