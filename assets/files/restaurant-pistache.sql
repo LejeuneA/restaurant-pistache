266c4e0fd8b99eca0a86fc8cd397348f12f82b53
A.Lejeune <acelyalejeune@gmail.com>
2024-06-13 15:06:08 +0200

    Update restaurant-pistache.sql

--- assets/files/restaurant-pistache.sql
+++ assets/files/restaurant-pistache.sql
@@ -54,7 +54,7 @@ CREATE TABLE `contact` (
   `phone` varchar(150) DEFAULT NULL,
   `message` varchar(500) NOT NULL,
   PRIMARY KEY (`idContact`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -63,7 +63,7 @@ CREATE TABLE `contact` (
 
 LOCK TABLES `contact` WRITE;
 /*!40000 ALTER TABLE `contact` DISABLE KEYS */;
-INSERT INTO `contact` VALUES (1,'Açelya','LEJEUNE','acelyalejeune@gmail.com','0493387729','svdsdsd'),(2,'John','Doe','john.doe@example.com','555-1234','Looking forward to visiting your restaurant next week!'),(3,'Jane','Smith','jane.smith@example.com','555-5678','I would like to book a table for four on Saturday.'),(4,'Emily','Jones','emily.jones@example.com','555-8765','What are your opening hours on weekends?'),(5,'Michael','Brown','michael.brown@example.com','555-4321','Do you offer vegetarian options on your menu?'),(6,'Jessica','Williams','jessica.williams@example.com','555-3456','Can I host a private event at your restaurant?'),(7,'David','Taylor','david.taylor@example.com','555-6543','Is there a dress code for dining at your place?'),(8,'Sarah','Johnson','sarah.johnson@example.com','555-7890','Can I see the menu before making a reservation?'),(9,'Chris','Lee','chris.lee@example.com','555-0987','What safety measures are in place due to COVID-19?'),(10,'Amanda','Miller','amanda.miller@example.com','555-2345','Do you have any special offers for new customers?'),(11,'James','Davis','james.davis@example.com','555-6789','Are you hiring any staff at the moment?'),(12,'Olivia','Martinez','olivia.martinez@example.com','555-1239','Can you accommodate gluten-free diets?'),(13,'Daniel','Garcia','daniel.garcia@example.com','555-9876','What is your policy on bringing pets to the restaurant?'),(14,'Sophia','Anderson','sophia.anderson@example.com','555-5432','Is there a parking facility available nearby?'),(15,'Matthew','Hernandez','matthew.hernandez@example.com','555-6547','Do you provide any loyalty programs for frequent diners?'),(16,'Emma','Rodriguez','emma.rodriguez@example.com','555-7654','Can I pre-order food before arriving at the restaurant?'),(17,'Joshua','Martinez','joshua.martinez@example.com','555-0981','Is there an outdoor seating area available?'),(18,'Ava','Lopez','ava.lopez@example.com','555-1324','What are your most popular dishes?'),(19,'Benjamin','Gonzalez','benjamin.gonzalez@example.com','555-5643','Do you offer any discounts for large groups?'),(20,'Isabella','Wilson','isabella.wilson@example.com','555-8760','Can I get a gift card for your restaurant?'),(21,'Mason','Martinez','mason.martinez@example.com','555-3412','Do you have live music on weekends?'),(22,'Açelya','LEJEUNE','acelyalejeune@gmail.com','0493387729','test');
+INSERT INTO `contact` VALUES (3,'Jane','Smith','jane.smith@example.com','555-5678','I would like to book a table for four on Saturday.'),(4,'Emily','Jones','emily.jones@example.com','555-8765','What are your opening hours on weekends?'),(5,'Michael','Brown','michael.brown@example.com','555-4321','Do you offer vegetarian options on your menu?'),(6,'Jessica','Williams','jessica.williams@example.com','555-3456','Can I host a private event at your restaurant?'),(7,'David','Taylor','david.taylor@example.com','555-6543','Is there a dress code for dining at your place?'),(8,'Sarah','Johnson','sarah.johnson@example.com','555-7890','Can I see the menu before making a reservation?'),(9,'Chris','Lee','chris.lee@example.com','555-0987','What safety measures are in place due to COVID-19?'),(10,'Amanda','Miller','amanda.miller@example.com','555-2345','Do you have any special offers for new customers?'),(11,'James','Davis','james.davis@example.com','555-6789','Are you hiring any staff at the moment?'),(12,'Olivia','Martinez','olivia.martinez@example.com','555-1239','Can you accommodate gluten-free diets?'),(13,'Daniel','Garcia','daniel.garcia@example.com','555-9876','What is your policy on bringing pets to the restaurant?'),(14,'Sophia','Anderson','sophia.anderson@example.com','555-5432','Is there a parking facility available nearby?'),(15,'Matthew','Hernandez','matthew.hernandez@example.com','555-6547','Do you provide any loyalty programs for frequent diners?'),(16,'Emma','Rodriguez','emma.rodriguez@example.com','555-7654','Can I pre-order food before arriving at the restaurant?'),(17,'Joshua','Martinez','joshua.martinez@example.com','555-0981','Is there an outdoor seating area available?'),(18,'Ava','Lopez','ava.lopez@example.com','555-1324','What are your most popular dishes?'),(19,'Benjamin','Gonzalez','benjamin.gonzalez@example.com','555-5643','Do you offer any discounts for large groups?'),(20,'Isabella','Wilson','isabella.wilson@example.com','555-8760','Can I get a gift card for your restaurant?'),(21,'Mason','Martinez','mason.martinez@example.com','555-3412','Do you have live music on weekends?');
 /*!40000 ALTER TABLE `contact` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -145,7 +145,7 @@ CREATE TABLE `reservations` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `active` tinyint(1) DEFAULT '1',
   PRIMARY KEY (`idReservation`)
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -223,4 +223,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-13 14:23:42
+-- Dump completed on 2024-06-13 15:04:57
