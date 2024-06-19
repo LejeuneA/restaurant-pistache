7ac688dad6366a1e6c12858286ebfce7a7893e94
A.Lejeune <acelyalejeune@gmail.com>
2024-06-19 10:59:35 +0200

    Update restaurant-pistache.sql

--- assets/files/restaurant-pistache.sql
+++ assets/files/restaurant-pistache.sql
@@ -54,7 +54,7 @@ CREATE TABLE `contact` (
   `phone` varchar(150) DEFAULT NULL,
   `message` varchar(500) NOT NULL,
   PRIMARY KEY (`idContact`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -145,7 +145,7 @@ CREATE TABLE `reservations` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `active` tinyint(1) DEFAULT '1',
   PRIMARY KEY (`idReservation`)
-) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=76 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -154,7 +154,7 @@ CREATE TABLE `reservations` (
 
 LOCK TABLES `reservations` WRITE;
 /*!40000 ALTER TABLE `reservations` DISABLE KEYS */;
-INSERT INTO `reservations` VALUES (13,'John Doe','john@example.com','1234567890','2024-06-12','18:00:00',4,'2024-06-12 10:57:31',1),(14,'Jane Smith','jane@example.com','9876543210','2024-06-13','19:00:00',2,'2024-06-12 10:57:31',1),(15,'Michael Brown','michael@example.com','5556667777','2024-06-14','20:00:00',3,'2024-06-12 10:57:31',0),(16,'Emily Johnson','emily@example.com','4443332222','2024-06-15','21:00:00',5,'2024-06-12 10:57:31',1),(17,'Alex Turner','alex@example.com','9998887777','2024-06-16','22:00:00',2,'2024-06-12 10:57:31',1),(18,'Sophia Garcia','sophia@example.com','1112223333','2024-06-17','18:30:00',3,'2024-06-12 10:57:31',0),(19,'William Wilson','william@example.com','4445556666','2024-06-18','19:30:00',4,'2024-06-12 10:57:31',1),(20,'Olivia Brown','olivia@example.com','7778889999','2024-06-19','20:30:00',2,'2024-06-12 10:57:31',1),(21,'James Martinez','james@example.com','2223334444','2024-06-20','21:30:00',3,'2024-06-12 10:57:31',0),(22,'Amelia Johnson','amelia@example.com','5554443333','2024-06-21','22:30:00',5,'2024-06-12 10:57:31',1),(23,'Michael Harris','michael@example.com','9991112222','2024-06-22','18:45:00',2,'2024-06-12 10:57:31',1),(24,'Emma Wilson','emma@example.com','6665554444','2024-06-23','19:45:00',3,'2024-06-12 10:57:31',0),(25,'Ethan Thomas','ethan@example.com','1112223333','2024-06-24','20:45:00',4,'2024-06-12 10:57:31',1),(26,'Isabella Anderson','isabella@example.com','3334445555','2024-06-25','21:45:00',2,'2024-06-12 10:57:31',1),(27,'Ava Jackson','ava@example.com','8889990000','2024-06-26','22:45:00',3,'2024-06-12 10:57:31',0),(28,'Noah White','noah@example.com','4445556666','2024-06-27','18:15:00',5,'2024-06-12 10:57:31',1),(29,'Sophia Brown','sophia@example.com','5556667777','2024-06-28','19:15:00',2,'2024-06-12 10:57:31',1),(30,'Benjamin Lee','benjamin@example.com','1112223333','2024-06-29','20:15:00',3,'2024-06-12 10:57:31',0),(31,'Mia Taylor','mia@example.com','4445556666','2024-06-30','21:15:00',4,'2024-06-12 10:57:31',1),(32,'Alexander Johnson','alexander@example.com','1112223333','2024-07-01','22:15:00',2,'2024-06-12 10:57:31',1),(36,'Acelya Lejeune','acelya@example.com','1436343333','2024-07-27','22:15:00',2,'2024-06-12 13:08:31',1),(37,'ACELYA LEJEUNE','acelyalejeune@gmail.com','0493387729','2024-06-27','17:54:00',1,'2024-06-13 11:50:48',1);
+INSERT INTO `reservations` VALUES (13,'John Doe','john@example.com','1234567890','2024-06-12','18:00:00',4,'2024-06-12 10:57:31',1),(14,'Jane Smith','jane@example.com','9876543210','2024-06-13','19:00:00',2,'2024-06-12 10:57:31',1),(15,'Michael Brown','michael@example.com','5556667777','2024-06-14','20:00:00',3,'2024-06-12 10:57:31',0),(16,'Emily Johnson','emily@example.com','4443332222','2024-06-15','21:00:00',5,'2024-06-12 10:57:31',1),(17,'Alex Turner','alex@example.com','9998887777','2024-06-16','22:00:00',2,'2024-06-12 10:57:31',1),(18,'Sophia Garcia','sophia@example.com','1112223333','2024-06-17','18:30:00',3,'2024-06-12 10:57:31',0),(19,'William Wilson','william@example.com','4445556666','2024-06-18','19:30:00',4,'2024-06-12 10:57:31',1),(20,'Olivia Brown','olivia@example.com','7778889999','2024-06-19','20:30:00',2,'2024-06-12 10:57:31',1),(21,'James Martinez','james@example.com','2223334444','2024-06-20','21:30:00',3,'2024-06-12 10:57:31',0),(22,'Amelia Johnson','amelia@example.com','5554443333','2024-06-21','22:30:00',5,'2024-06-12 10:57:31',1),(23,'Michael Harris','michael@example.com','9991112222','2024-06-22','18:45:00',2,'2024-06-12 10:57:31',1),(24,'Emma Wilson','emma@example.com','6665554444','2024-06-23','19:45:00',3,'2024-06-12 10:57:31',0),(25,'Ethan Thomas','ethan@example.com','1112223333','2024-06-24','20:45:00',4,'2024-06-12 10:57:31',1),(26,'Isabella Anderson','isabella@example.com','3334445555','2024-06-25','21:45:00',2,'2024-06-12 10:57:31',1),(27,'Ava Jackson','ava@example.com','8889990000','2024-06-26','22:45:00',3,'2024-06-12 10:57:31',0),(28,'Noah White','noah@example.com','4445556666','2024-06-27','18:15:00',5,'2024-06-12 10:57:31',1),(29,'Sophia Brown','sophia@example.com','5556667777','2024-06-28','19:15:00',2,'2024-06-12 10:57:31',1),(30,'Benjamin Lee','benjamin@example.com','1112223333','2024-06-29','20:15:00',3,'2024-06-12 10:57:31',0),(31,'Mia Taylor','mia@example.com','4445556666','2024-06-30','21:15:00',4,'2024-06-12 10:57:31',1),(32,'Alexander Johnson','alexander@example.com','1112223333','2024-07-01','22:15:00',2,'2024-06-12 10:57:31',1),(72,'ACELYA LEJEUNE','acelyalejeune@gmail.com','0493387729','2024-06-23','03:16:00',4,'2024-06-13 22:45:27',1);
 /*!40000 ALTER TABLE `reservations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -175,7 +175,7 @@ CREATE TABLE `starters` (
   `active` tinyint(1) DEFAULT '1',
   `idCategory` int DEFAULT '1',
   PRIMARY KEY (`idStarter`)
-) ENGINE=InnoDB AUTO_INCREMENT=8002 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8003 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -223,4 +223,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-13 15:04:57
+-- Dump completed on 2024-06-19 10:15:26
